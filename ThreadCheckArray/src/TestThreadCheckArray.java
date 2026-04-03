8f2e02623d3a95efd5470813f24697fea06d1663
Haytam <Haytam@host.docker.internal>
2026-04-03 12:13:53 +0300

    TestThread Update to ArrayList

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class TestThreadCheckArray {
 	public static void main(String[] args) {
@@ -6,16 +8,16 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+            ArrayList<Integer> arrayList = new ArrayList<>();
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+                arrayList.add(input.nextInt());
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
 			
-			SharedData sd = new SharedData(array, num);
+			SharedData sd = new SharedData(arrayList, num);
 			
 			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
 			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
@@ -35,37 +37,34 @@ public class TestThreadCheckArray {
 				System.out.println("Sorry");
 				return;
 			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
+            System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArrayList().size());
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+            for (int index = 0; index < sd.getArrayList().size(); index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
-			for (int index : sd.getArray())
-			{
-				System.out.print(index);
-				int counter = 5;
-				while (true)
-				{
-					index = index / 10;
-					counter--;
-					if (index == 0)
-						break;
-				}
-				for (int i = 0; i < counter; i++)
-					System.out.print(" ");
-			}
+            for (int value : sd.getArrayList()) {
+                System.out.print(value);
+                int counter = 5;
+                int temp = value;
+                while (true) {
+                    temp = temp / 10;
+                    counter--;
+                    if (temp == 0)
+                        break;
+                }
+                for (int i = 0; i < counter; i++)
+                    System.out.print(" ");
+            }
 
-			System.out.println();
-			System.out.print("C:    ");
-			for (boolean index : sd.getWinArray())
-			{
-				if (index)
-					System.out.print("1    ");
-				else
-					System.out.print("0    ");	
-			}
-		}
-	}
-
+            System.out.println();
+            System.out.print("C:    ");
+            for (boolean index : sd.getWinArray()) {
+                if (index)
+                    System.out.print("1    ");
+                else
+                    System.out.print("0    ");
+            }
+        }
+    }
 }
